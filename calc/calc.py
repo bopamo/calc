c186926778b661f1df9e49023cabca0da02144fa
Bopamo Osaisai <bopamo@gmail.com>
2019-09-01 13:42:14 -0700

    Implemented and tested subtraction function

--- calc/calc.py
+++ calc/calc.py
@@ -10,3 +10,6 @@ class Calc:
 
     def add(self, *args):
         return sum(args)
+
+    def sub(self, a, b):
+        return a - b
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -33,3 +33,19 @@ def test_add_many_numbers():
     s = range(100)
 
     assert Calc().add(*s) == 4950
+
+
+def test_subtract_two_numbers():
+    c = Calc()
+
+    res = c.sub(10, 3)
+
+    assert res == 7
+
+
+def test_subtract_two_numbers_negative():
+    c = Calc()
+
+    res = c.sub(9999, 9999999111111)
+
+    assert res == 9999 - 9999999111111
